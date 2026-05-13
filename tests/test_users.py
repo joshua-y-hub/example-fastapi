32af22c2c1213ccf8b77d268145748aa6d2b4bc6
joshua-y-hub <joshuamakau917@gmail.com>
2026-05-13 14:02:55 +0300

    user table test

---
+++ tests/test_users.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_root():
+    res = client.get("/")
+    print(res.json().get("Hello"))
+    assert res.json().get("Hello") == "World"
+    assert res.status_code == 200
+
+def test_create_user():
+    res = client.post("/users/", json={ "email": "john@example.com","password": "pass123"})
+    print(res.json())
+    assert res.status_code == 201
+  
